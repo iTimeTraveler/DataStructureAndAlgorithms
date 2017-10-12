eeb0c9396babccc3161df13fdfc0e31a83bdaf73
iTimeTraveler <xuewenlong.2008@gmail.com>
2017-10-12 19:24:59 +0800

    Solution: find two integers, such that sum is equals to N.（Bitmap Algorithm）

--- src/main/java/algorithms/TwoSum.java
+++ src/main/java/algorithms/TwoSum.java
@@ -17,8 +17,14 @@ public class TwoSum {
 		}
 		System.out.println(Arrays.toString(arr));
 		System.out.println(Integer.BYTES * arr.length + "byte");
+
+		//哈希表
 		int[] result = findTwoIntSumEqualsN(arr, 1005);
-		System.out.println("result: " + Arrays.toString(result));
+		System.out.println("Hashmap result: " + Arrays.toString(result));
+
+		//位图法
+		int[] result2 = findTwoSumByBitmap(arr, 1005);
+		System.out.println("Bitmap result: " + Arrays.toString(result2));
 	}
 
 	/**
@@ -34,10 +40,38 @@ public class TwoSum {
 			int complement = n - value;
 			if(map.containsKey(complement)){
 				System.out.println("Two indexes are: " + i + "," + map.get(complement));
-				return new int[]{value, complement};
+				return new int[]{complement, value};
 			}
 			map.put(value, i);
 		}
 		throw new IllegalArgumentException("No two sum solution");
+	}
+
+	/**
+	 * 位图法
+	 */
+	public static int[] findTwoSumByBitmap(int[] arr, int n){
+		int bitmap[] = new int[0x20000000];		//2^29
+
+		for(int i = 0; i < arr.length; i++){
+			int value = arr[i];
+			if(value > n) continue;
+
+			int complement = n - value;
+			int exist = getBit(bitmap, complement);
+			if(exist != 0){		//is exist
+				return new int[]{complement, value};
+			}
+			setBit(bitmap, arr[i]);
+		}
+		throw new IllegalArgumentException("No two sum solution by bitmap.");
+	}
+
+	public static void setBit(int bitArray[], int k){
+		bitArray[k / Byte.SIZE] |= (1 << (k % Byte.SIZE));
+	}
+
+	public static int getBit(int bitArray[], int k){
+		return (bitArray[k / Byte.SIZE] & (1 << (k % Byte.SIZE)));
 	}
 }
