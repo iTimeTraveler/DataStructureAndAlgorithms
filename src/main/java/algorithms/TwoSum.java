0535d633424a91dd50bf3b74cc1f09d4f90bc5ba
iTimeTraveler <xuewenlong.2008@gmail.com>
2017-10-13 18:06:07 +0800

    optimize TwoSum.

--- src/main/java/algorithms/TwoSum.java
+++ src/main/java/algorithms/TwoSum.java
@@ -5,22 +5,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TwoSum {
-	private static final int ARRAY_LENGTH = 10000000;
-	private static final int MAX_VALUE = 9000;
+	private static final int RANDOM_ARRLEN = 100000000;
+	private static final int RANDOM_MAX_VALUE = 90000000;
+	private static final int BITMAP_MAXLEN = 0x20000000;			//2^29
 
 	public static void main(String args[]){
 
 //		int[] arr = new int[]{9, 8, 7, 6, 5, 4, 3, 12, 11};
-		int[] arr = new int[ARRAY_LENGTH];
-		for(int i = 0; i < ARRAY_LENGTH; i++){
-			arr[i] = (int)(Math.random() * MAX_VALUE);
+		int[] arr = new int[RANDOM_ARRLEN];
+		for(int i = 0; i < RANDOM_ARRLEN; i++){
+			arr[i] = (int)(Math.random() * RANDOM_MAX_VALUE);
 		}
-		System.out.println(Arrays.toString(arr));
+//		System.out.println(Arrays.toString(arr));
 		System.out.println(Integer.BYTES * arr.length + "byte");
 
 		//哈希表
-		int[] result = findTwoIntSumEqualsN(arr, 1005);
-		System.out.println("Hashmap result: " + Arrays.toString(result));
+//		int[] result = findTwoIntSumEqualsN(arr, 1005);
+//		System.out.println("Hashmap result: " + Arrays.toString(result));
 
 		//位图法
 		int[] result2 = findTwoSumByBitmap(arr, 1005);
@@ -51,7 +52,7 @@ public class TwoSum {
 	 * 位图法
 	 */
 	public static int[] findTwoSumByBitmap(int[] arr, int n){
-		int bitmap[] = new int[0x20000000];		//2^29
+		byte bitmap[] = new byte[n / Byte.SIZE];
 
 		for(int i = 0; i < arr.length; i++){
 			int value = arr[i];
@@ -67,11 +68,11 @@ public class TwoSum {
 		throw new IllegalArgumentException("No two sum solution by bitmap.");
 	}
 
-	public static void setBit(int bitArray[], int k){
-		bitArray[k / Integer.SIZE] |= (1 << (k % Integer.SIZE));
+	public static void setBit(byte bitmap[], int k){
+		bitmap[k / Byte.SIZE] |= (1 << (k % Byte.SIZE));
 	}
 
-	public static int getBit(int bitArray[], int k){
-		return (bitArray[k / Integer.SIZE] & (1 << (k % Integer.SIZE)));
+	public static int getBit(byte bitmap[], int k){
+		return (bitmap[k / Byte.SIZE] & (1 << (k % Byte.SIZE)));
 	}
 }
